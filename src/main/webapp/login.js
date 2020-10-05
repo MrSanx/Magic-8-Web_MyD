8b2c087fbb6fa2d1ed337d03340093605b1bdcf1
MiguelFaCa <71671392+MiguelFaCa@users.noreply.github.com>
2020-10-05 15:52:55 -0500

    JavaScrip & Servlet

--- src/main/webapp/login.js
+++ src/main/webapp/login.js
@@ -1,8 +1,42 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+var request, formulario_formu;
+
+formulario_formu = document.forms["formu"];
+
+formulario_formu.addEventListener("submit", cargarInfo, false); //Cada vez que se le de a submit, se va a prevenir que se mande
+
+function cargarInfo(e) {
+    e.preventDefault();
+    inicializarXHMHttpRequest();
+
+    request.onreadystatechange = ProcesaRespuesta;
+    request.open("POST", "servicioLogin", true);
+    //1. Tipo de peticion, si es post o get
+    //2.  Url
+    //3. La peticion sera asincrona 
+    request.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+    //Especifica la cadena de peticion
+    request.send(crear_query_String());
+
+}
+function inicializarXHMHttpRequest() {
+    request = new XMLHttpRequest(); //Inicializa la variable request con el operador new
+}
 
 
-//jajaj hola
+
+function crear_query_String() {
+    let username = formulario_formu.elements["username"].value;
+    let password = formulario_formu.elements["password"].value;
+ 
+
+
+    return "username=" + encodeURI(username) +
+            "&password=" + encodeURI(password);
+}
+function ProcesaRespuesta() {
+    if (request.readyState === 4 & request.status === 200) {
+        console.log(request.response);
+        
+    }
+}
